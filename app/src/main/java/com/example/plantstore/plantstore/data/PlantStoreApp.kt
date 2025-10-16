873d39df9bc1c85287fd1f6229ea49ca3d4b36df
Alonza <a.lozova.s@gmail.com>
2025-10-16 18:57:08 +0300

    App для Hilt

---
+++ app/src/main/java/com/example/plantstore/plantstore/data/PlantStoreApp.kt
@@ -0,0 +1,8 @@
+package com.example.plantstore.plantstore.data
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PlantStoreApp : Application() {
+}
