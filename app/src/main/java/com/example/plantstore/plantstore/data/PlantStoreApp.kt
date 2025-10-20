6ed808686222038b6d1d3e1383078d14349a8f4d
Alonza <a.lozova.s@gmail.com>
2025-10-20 13:49:30 +0300

    Delete

--- app/src/main/java/com/example/plantstore/plantstore/data/PlantStoreApp.kt
+++
@@ -1,8 +0,0 @@
-package com.example.plantstore.plantstore.data
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class PlantStoreApp : Application() {
-}
